cc5be14192c4c9020c61a0ec14a462f06d40ff35
Jacob Homanics <homanicsjake@gmail.com>
2024-12-21 17:00:46 -0600

    up

--- Assets/Raycast Controller/Scripts/RaycastController.cs
+++ Assets/Raycast Controller/Scripts/RaycastController.cs
@@ -1,51 +1,62 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class RaycastController : MonoBehaviour
+namespace JacobHomanics.Core.Raycasts
 {
-    [Header("Configuration")]
-    public float distance = Mathf.Infinity;
-    public LayerMask layerMask = ~0;
-
-    public bool castOnUpdate = true;
-
-    [Header("Debug")]
-    public Color rayColor = Color.red;
-    public Color rayHitColor = Color.green;
-    public Color sphereColor = Color.blue;
-    public float hitSphereSize = 0.25f;
-
-    [Header("Events")]
-    public UnityEvent<Collider> OnEnter = new();
-    public UnityEvent<Collider> OnStay = new();
-    public UnityEvent<Collider> OnExit = new();
-
-    public Collider LastHit { get; private set; }
-
-    private Ray ray;
-    private RaycastHit hit;
-
-    void Update()
+    public class RaycastController : MonoBehaviour
     {
-        if (!castOnUpdate) return;
+        [Header("Configuration")]
+        public float distance = Mathf.Infinity;
+        public LayerMask layerMask = ~0;
 
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, distance, layerMask))
+        public bool castOnUpdate = true;
+
+        [Header("Debug")]
+        public Color rayColor = Color.red;
+        public Color rayHitColor = Color.green;
+        public Color sphereColor = Color.blue;
+        public float hitSphereSize = 0.25f;
+
+        [Header("Events")]
+        public UnityEvent<Collider> OnEnter = new();
+        public UnityEvent<Collider> OnStay = new();
+        public UnityEvent<Collider> OnExit = new();
+
+        public Collider LastHit { get; private set; }
+
+        private Ray ray;
+        private RaycastHit hit;
+
+        void Update()
         {
-            if (hit.collider != null)
+            if (!castOnUpdate) return;
+
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, distance, layerMask))
             {
-                if (hit.collider != LastHit)
+                if (hit.collider != null)
                 {
-                    if (LastHit != null)
-                        OnExit?.Invoke(LastHit);
+                    if (hit.collider != LastHit)
+                    {
+                        if (LastHit != null)
+                            OnExit?.Invoke(LastHit);
 
-                    OnEnter?.Invoke(hit.collider);
+                        OnEnter?.Invoke(hit.collider);
 
-                    LastHit = hit.collider;
+                        LastHit = hit.collider;
+                    }
+                    else
+                    {
+                        OnStay?.Invoke(hit.collider);
+                    }
                 }
                 else
                 {
-                    OnStay?.Invoke(hit.collider);
+                    if (LastHit != null)
+                    {
+                        OnExit?.Invoke(LastHit);
+                        LastHit = null;
+                    }
                 }
             }
             else
@@ -57,33 +68,25 @@ public class RaycastController : MonoBehaviour
                 }
             }
         }
-        else
+
+        void OnDrawGizmos()
         {
-            if (LastHit != null)
+            if (Camera.main == null)
+                return;
+
+            float maxDistance = Mathf.Min(distance, 10000f);
+
+            Gizmos.color = rayColor;
+            Gizmos.DrawRay(ray.origin, ray.direction * maxDistance);
+
+            // Draw the hit point
+            if (hit.collider != null)
             {
-                OnExit?.Invoke(LastHit);
-                LastHit = null;
+                Gizmos.color = rayHitColor;
+                Gizmos.DrawLine(ray.origin, hit.point);
+                Gizmos.color = sphereColor;
+                Gizmos.DrawWireSphere(hit.point, 0.25f); // Sphere size is adjustable
             }
-        }
-    }
-
-    void OnDrawGizmos()
-    {
-        if (Camera.main == null)
-            return;
-
-        float maxDistance = Mathf.Min(distance, 10000f);
-
-        Gizmos.color = rayColor;
-        Gizmos.DrawRay(ray.origin, ray.direction * maxDistance);
-
-        // Draw the hit point
-        if (hit.collider != null)
-        {
-            Gizmos.color = rayHitColor;
-            Gizmos.DrawLine(ray.origin, hit.point);
-            Gizmos.color = sphereColor;
-            Gizmos.DrawWireSphere(hit.point, 0.25f); // Sphere size is adjustable
         }
     }
 }
